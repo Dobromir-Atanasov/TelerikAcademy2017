8552ccb13840c7746a6a4c968ddda178b60b7843
Dobromir Atanasov <dobromir.atanasov@mail.bg>
2017-11-23 19:11:05 +0200

    done at TA

---
+++ Nov23/matrixes/MaximalSum/MaximalSum.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaximalSum
+{
+	class MaximalSum
+	{
+		public static int[,] matrix;
+		static void Main(string[] args)
+		{
+			string[] nAndM = Console.ReadLine().Split(' ');
+			int n = int.Parse(nAndM[0]);
+			int m = int.Parse(nAndM[1]);
+			matrix = new int[n, m];
+			for (int i = 0; i < n; i++)
+			{
+				string[] temp = Console.ReadLine().Split(' ');
+				for (int j = 0; j < temp.Length; j++)
+				{
+					matrix[i, j] = int.Parse(temp[j]);
+					//Console.Write(matrix[i, j] + " ");
+				}
+				//Console.WriteLine();
+			}
+			int maxSum = GetMaxSum(0, 0);
+			for (int i = 0; i < n-2; i++)
+			{
+				for (int j = 0; j < m-2; j++)
+				{
+					if (GetMaxSum(i,j)>maxSum)
+					{
+						maxSum = GetMaxSum(i, j);
+					}
+				}
+			}
+			Console.WriteLine(maxSum);
+		}
+
+		static int GetMaxSum(int i, int j) {
+			return matrix[i, j] + matrix[i + 1, j] + matrix[i + 2, j] +
+				matrix[i, j + 1] + matrix[i + 1, j + 1] + matrix[i + 2, j + 1] +
+				matrix[i, j + 2] + matrix[i + 1, j + 2] + matrix[i + 2, j + 2];
+		}
+	}
+}
---
+++ Nov23/matrixes/matrixes/FillTheMatrix.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace matrixes
+{
+	class FillTheMatrix
+	{
+		public static int[,] matrix;
+		public static int mSize;
+		static void Main(string[] args)
+		{
+			mSize = int.Parse(Console.ReadLine());
+			string fillType = Console.ReadLine();
+
+			matrix = new int[mSize, mSize];
+			switch (fillType)
+			{
+				case "a":
+					CaseA();
+					break;
+				case "b":
+					CaseB();
+					break;
+				case "c":
+					CaseC();
+					break;
+				case "d":
+					CaseD();
+					break;
+				default:
+					break;
+			}
+			if (fillType != "d")
+			{
+				PrintMatrix();
+			}
+		}
+
+		static void CaseC()
+		{
+			//implement
+		}
+		static void PrintMatrix()
+		{
+			for (int i = 0; i < mSize; i++)
+			{
+				for (int j = 0; j < mSize; j++)
+				{
+					Console.Write(matrix[i, j]);
+					if (j != mSize - 1)
+					{
+						Console.Write(" ");
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+
+		static void CaseD()
+		{
+			int n = mSize;
+
+			int k = 1, c1 = 0, c2 = n - 1, r1 = 0, r2 = n - 1;
+
+			while (k <= n * n)
+			{
+				for (int i = c1; i <= c2; i++)
+				{
+					matrix[r1, i] = k++;
+				}
+
+				for (int j = r1 + 1; j <= r2; j++)
+				{
+					matrix[j, c2] = k++;
+				}
+
+				for (int i = c2 - 1; i >= c1; i--)
+				{
+					matrix[r2, i] = k++;
+				}
+
+				for (int j = r2 - 1; j >= r1 + 1; j--)
+				{
+					matrix[j, c1] = k++;
+				}
+
+				c1++;
+				c2--;
+				r1++;
+				r2--;
+			}
+			for (int i = 0; i < mSize; i++)
+			{
+				for (int j = 0; j < mSize; j++)
+				{
+					Console.Write(matrix[j, i]);
+					if (j != mSize - 1)
+					{
+						Console.Write(" ");
+					}
+				}
+				Console.WriteLine();
+			}
+		}
+
+		static void CaseB()
+		{
+			int counter = 1;
+			for (int i = 0; i < mSize; i++)
+			{
+				if (i % 2 == 0)
+				{
+					for (int j = 0; j < mSize; j++)
+					{
+						matrix[j, i] = counter++;
+					}
+				}
+				else
+				{
+					for (int j = mSize - 1; j >= 0; j--)
+					{
+						matrix[j, i] = counter++;
+					}
+				}
+			}
+		}
+
+		static void CaseA()
+		{
+			int counter = 1;
+			for (int i = 0; i < mSize; i++)
+			{
+				for (int j = 0; j < mSize; j++)
+				{
+					matrix[j, i] = counter;
+					counter++;
+				}
+			}
+		}
+	}
+}
