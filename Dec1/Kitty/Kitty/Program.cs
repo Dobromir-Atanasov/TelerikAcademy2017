082857f50baca4d8ac52edebf79d3e27f77d6c7e
Dobromir Atanasov <dobromir.atanasov@mail.bg>
2017-12-04 09:56:22 +0200

    updates

---
+++ Dec1/Kitty/Kitty/Program.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kitty
+{
+	class Program
+	{
+		public static int soulsCounter = 0;
+		public static int foodCounter = 0;
+		public static int deadlockCounter = 0;
+		public static char[] maze;
+		public static int[] moves;
+		public static int currentPosition = 0;
+		public static int jumps = 0;
+		public static bool exitTime = false;
+
+		static void Main(string[] args)
+		{
+			maze = Console.ReadLine()
+				.ToCharArray();
+			moves = Console.ReadLine()
+				.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+			CheckPosition(currentPosition);
+			for (int move = 0; move < moves.Length; move++)
+			{
+				if (exitTime)
+				{
+					Console.WriteLine("You are deadlocked, you greedy kitty!");
+					Console.WriteLine("Jumps before deadlock: {0}", jumps);
+					break;
+				}
+				jumps++;
+
+				//COMENT ME
+				//PrintStats();
+				if (moveToRight(move))
+				{
+					currentPosition+=moves[move]%maze.Length;
+					if (currentPosition>maze.Length-1)
+					{
+						currentPosition -= maze.Length;
+					}
+				}
+				else
+				{
+					currentPosition+=moves[move]%maze.Length; //we add again since number is negative
+					if (currentPosition<0)
+					{
+						currentPosition += maze.Length;
+					}
+				}
+				CheckPosition(currentPosition);
+				if (exitTime)
+				{
+					Console.WriteLine("You are deadlocked, you greedy kitty!");
+					Console.WriteLine("Jumps before deadlock: {0}", jumps);
+					break;
+				}
+			}
+			if (!exitTime)
+			{
+				PrintStats();
+			}
+		}
+
+		private static bool moveToRight(int move)
+		{
+			return moves[move] >= 0 ? true : false;
+		}
+
+		static void CheckPosition(int position)
+		{
+			char posElement = maze[position];
+			if (posElement == '@')
+			{
+				soulsCounter++;
+				maze[position] = 'N';
+				return;
+			}
+			if (posElement == '*')
+			{
+				foodCounter++;
+				maze[position] = 'N';
+				return;
+			}
+			if (posElement == 'x')
+			{
+				DeadTrap(position);
+				return;
+			}
+			if (posElement == 'N')
+			{
+				return;
+			}
+		}
+		public static void PrintStats()
+		{
+			Console.WriteLine("Coder souls collected: {0}", soulsCounter);
+			Console.WriteLine("Food collected: {0}", foodCounter);
+			Console.WriteLine("Deadlocks: {0}", deadlockCounter);
+			//FOR TEST ONLY!!!
+			//PrintMaze();
+		}
+
+		public static void PrintMaze()
+		{
+			Console.WriteLine(string.Join(" ", maze));
+			Console.WriteLine("Position {0}", currentPosition);
+		}
+
+		private static void DeadTrap(int position)
+		{
+			deadlockCounter++;
+			if (position%2==0)
+			{
+				//must leave soul
+				if (soulsCounter>=1)
+				{
+					soulsCounter--;
+					maze[position] = '@';
+				}
+				else
+				{
+					exitTime = true;
+				}
+			}
+			else
+			{
+				//must leave food
+				if (foodCounter >= 1)
+				{
+					foodCounter--;
+					maze[position] = '*';
+				}
+				else
+				{
+					exitTime = true;
+				}
+			};
+		}
+	}
+}
---
+++ Dec1/Kitty/LargestAreaInMatrix/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace LargestAreaInMatrix
+{
+	class Program
+	{
+		public static BigInteger result = 0;
+		public static int currentRow = 0;
+		public static int currentCol = 0;
+		public static BigInteger[,] matrix;
+		static void Main(string[] args)
+		{
+			int rows = int.Parse(Console.ReadLine());
+			int cols = int.Parse(Console.ReadLine());
+			currentRow = rows - 1;
+			matrix = new BigInteger[rows, cols];
+			FillMatrix(rows, cols, matrix);
+			//PrintMatrix(matrix, rows, cols);
+			int numberOfMoves = int.Parse(Console.ReadLine());
+			int[] moves = Console.ReadLine()
+				.Split(new[] { " " }, StringSplitOptions.RemoveEmptyEntries)
+				.Select(int.Parse)
+				.ToArray();
+			int coeff = Math.Max(rows, cols);
+			for (int i = 0; i < numberOfMoves; i++)
+			{
+				int targetRow = moves[i]/coeff;
+				int targetCol = moves[i] % coeff;
+				MoveToPosition(targetRow, targetCol);
+			}
+			Console.WriteLine(result);
+		}
+
+		private static void MoveToPosition(int targetRow, int targetCol)
+		{
+			if (currentCol<targetCol)
+			{
+				for (int i = currentCol; i <= targetCol; i++)
+				{
+					result += matrix[currentRow, i];
+					matrix[currentRow, i] = 0;
+					
+
+				}
+				currentCol = targetCol;
+			}
+			else
+			{
+				for (int i = targetCol; i <= currentCol; i++)
+				{
+					result += matrix[currentRow, i];
+					matrix[currentRow, i] = 0;
+				}
+				currentCol = targetCol;
+
+			}
+			if (currentRow<targetRow)
+			{
+				for (int i = currentRow; i <= targetRow; i++)
+				{
+					result += matrix[i,currentCol];
+					matrix[i, currentCol] = 0;
+				}
+				currentRow = targetRow;
+			}
+			else
+			{
+				for (int i = targetRow; i <= currentRow; i++)
+				{
+					result += matrix[i, currentCol];
+					matrix[i, currentCol] = 0;
+				}
+				currentRow = targetRow;
+			}
+		}
+
+		private static void PrintMatrix(BigInteger[,] matrix, int rows, int cols)
+		{
+			for (int i = 0; i < rows; i++)
+			{
+				for (int j = 0; j < cols; j++)
+				{
+					Console.Write(matrix[i, j] + " ");
+				}
+				Console.WriteLine();
+			};
+		}
+
+		private static void FillMatrix(int rows, int cols, BigInteger[,] matrix)
+		{
+			for (int row = rows-1; row >=0; row--)
+			{
+				for (int col = 0; col < cols; col++)
+				{
+					matrix[row, col] = (BigInteger)(Math.Pow(2, rows-row-1) * Math.Pow(2, col));
+				}
+			}
+		}
+
+	}
+}
